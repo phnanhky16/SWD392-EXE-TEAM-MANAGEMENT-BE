1ba3e8f95a92776ff1cd1b06c30bb27beccc88dd
phnanhky <kypase184884@fpt.edu.vn>
2025-09-22 22:27:56 +0700

    Add files via upload

---
+++ src/main/java/com/swd/exe/teammanagement/ExeTeamManagementApplication.java
@@ -0,0 +1,13 @@
+package com.swd.exe.teammanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeTeamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeTeamManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd/exe/teammanagement/ExeTeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd.exe.teammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeTeamManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
